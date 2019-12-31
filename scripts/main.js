adfed475b670e8c4eab082c4caa6efd570d378f2
Hans David Hellman <hanshellman@gmail.com>
2019-12-31 00:09:51 -0800

    final draft commit of this exercise

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,33 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
 
-let myVariable = 'Bob';
-myVariable = 'Steve';
-//this is a comment.
-/*
-This is also a comment.
-*/
-let iceCream = 'chocolate';
-if (iceCream === 'chocolate') {
-    alert('yes dude')
-} else {
-    alert('what the hell is this shit?')
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/250x250.png') {
+      myImage.setAttribute ('src','images/7-eleven.png');
+    } else {
+      myImage.setAttribute ('src','images/250x250.png');
+    }
 }
-document.querySelector('html').onclick = function() {
-    alert('Hey!')
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Music is cool, ' + myName;
+    }
+  }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+}   else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Music is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
 }
